01f75e14ab1f6a2a4aed06e8264f4c4292647e13
pogsdbest <arnel.espiritu@gmail.com>
2015-04-05 18:06:16 +0800

    first commit

---
+++ desktop/src/com/pogs/spaceimpactgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pogs.spaceimpactgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pogs.spaceimpactgame.SpaceImpactMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceImpactMain(), config);
+	}
+}
